3cf1d0e5f4149d7bb6cb3d6a6f94d7b83df5d58f
Jeffrey Zhang <jeffreyzhang519@outlook.com>
2025-01-03 00:28:04 -0800

    add typeorm

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,4 +1,4 @@
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from './task.model';
 import { IsOptional, IsEnum, IsString } from 'class-validator';
 
 export class GetTasksFilterDto {
